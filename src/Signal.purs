a5a5d7b02793bcb9d2fa7fc46a31ea3827a9785b
Josh Bassett <josh.bassett@gmail.com>
2019-01-14 17:11:32 +1100

    Make Signal an instance of the MonadCont type class

--- src/Signal.purs
+++ src/Signal.purs
@@ -1,19 +1,35 @@
-module Signal where
+module Signal
+  (
+    Signal
+  , SignalConstructor
+  , SignalT(..)
+  , runSignal
+  , module Control.Monad.Cont.Class
+  ) where
 
-import Control.Monad.Trans.Class (class MonadTrans)
-import Effect (Effect)
 import Prelude
 
--- | Represents a signal.
+import Control.Monad.Cont.Class (class MonadCont, callCC)
+import Control.Monad.Trans.Class (class MonadTrans)
+import Effect (Effect)
+
+-- | This signal monad transformer.
+-- |
+-- | This monad transform is based on the continuation monad.
 -- |
 -- | The type parameter `r` is the return type of the monad `m`. The callback
 -- | function takes a parameter `a`.
--- |
--- | This is based on the continuation monad.
 newtype SignalT a m r = SignalT ((r -> m a) -> m a)
 
 type Signal a = SignalT Unit Effect a
 type SignalConstructor a = (((a -> Effect Unit) -> Effect Unit) -> Signal a)
+
+-- Runs a signal with a given callback function.
+runSignal :: forall a m r. (Monad m) => SignalT a m r -> (r -> m a) -> m a
+runSignal (SignalT sf) f = sf f
+
+instance monadContSignalT :: Monad m => MonadCont (SignalT r m) where
+  callCC f = SignalT (\k -> runSignal (f (\a -> SignalT (\_ -> k a))) k)
 
 instance functorSignal :: (Monad m) => Functor (SignalT r m) where
   map f s = SignalT (\k -> runSignal s (\a -> k $ f a))
@@ -31,10 +47,3 @@ instance monadSignal :: (Monad m) => Monad (SignalT r m)
 
 instance monadTransSignal :: MonadTrans (SignalT r) where
   lift m = SignalT (\k -> m >>= k)
-
--- Runs a signal with a given callback function.
-runSignal :: forall a m r. (Monad m) => SignalT a m r -> (r -> m a) -> m a
-runSignal (SignalT sf) f = sf f
-
-callCC :: forall a b. ((a -> Signal b) -> Signal a) -> Signal a
-callCC f = SignalT (\k -> runSignal (f (\a -> SignalT (\_ -> k a))) k)

0245a6be3b12084493562d4eafe9addf5f72a013
Josh Bassett <josh.bassett@gmail.com>
2019-01-15 22:39:01 +1100

    Refactor scan function to use signal monad

--- src/Example8.purs
+++ src/Example8.purs
@@ -6,18 +6,17 @@ import Control.Monad.Trans.Class (lift)
 import Effect (Effect)
 import Effect.Console (logShow)
 import Effect.Ref as Ref
-import Signal (Signal, callCC, runSignal)
+import Signal (Signal, runSignal)
 import Signal.Timer (interval)
 
 scan :: forall a b. (b -> a -> b) -> b -> Signal a -> Signal b
 scan f x s = do
   current <- lift $ Ref.new x
-  callCC $ \k -> do
-    a <- s
-    b <- lift $ Ref.read current
-    let r = f b a
-    lift $ Ref.write r current
-    k r
+  a <- s
+  b <- lift $ Ref.read current
+  let r = f b a
+  lift $ Ref.write r current
+  pure r
 
 main :: Effect Unit
 main = do
